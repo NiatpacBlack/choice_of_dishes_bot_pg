eeee8c7a01e74b8ed5119c71e3b8bb407a49770f
niatpacblack <alexseeffwork@gmail.com>
2022-12-07 15:52:44 +0300

    Успешная проверка подключения к базе данных.

--- db.py
+++ db.py
@@ -1,1 +1,22 @@
+import os
+from contextlib import closing
+
 import psycopg2
+from dotenv import load_dotenv
+from psycopg2 import sql
+
+load_dotenv()
+
+with closing(psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'),
+                              password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'))) as conn:
+    with conn.cursor() as cursor:
+        columns = ('id', 'firstname', 'lastname', 'email', 'age')
+
+        stmt = sql.SQL('SELECT {} FROM {}').format(
+            sql.SQL(',').join(map(sql.Identifier, columns)),
+            sql.Identifier('test_table')
+        )
+        cursor.execute(stmt)
+
+        for row in cursor:
+            print(row)
