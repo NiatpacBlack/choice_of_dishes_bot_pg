c691dc5edc549157b184276b58d2bbaa1e0bfa57
Xero0a <chipsikchannel@gmail.com>
2022-12-10 18:57:55 +0300

    Исправил код.

--- db.py
+++ db.py
@@ -18,26 +18,30 @@ class PostgresClient:
         self.user = user
         self.password = password
         self.host = host
-        self.db_connect = psycopg2.connect(dbname=self.dbname, user=self.user,
-                                           password=self.password, host=self.host)
+        self.db_connect = psycopg2.connect(
+            dbname=self.dbname, user=self.user, password=self.password, host=self.host
+        )
         self.cursor = self.db_connect.cursor()
 
     def select_all_tables_name_from_db(self) -> List[Tuple[str, ...]]:
         """Выводит список кортежей, содержащий названия всех таблиц из базы данных."""
 
-        self.cursor.execute("""
+        self.cursor.execute(
+            """
                             SELECT table_name FROM information_schema.tables \
                             WHERE table_schema NOT IN ('information_schema','pg_catalog');
-                        """)
+                        """
+        )
 
         return self.cursor.fetchall()
 
-    def select_columns_from_table(self, table_name: str, *args: str) -> List[Tuple[str, ...]]:
+    def select_columns_from_table(
+        self, table_name: str, *args: str
+    ) -> List[Tuple[str, ...]]:
         """Возвращает список с кортежами, содержащими данные переданных полей из *args из таблицы table_name."""
 
-        query = sql.SQL('SELECT {} FROM {}').format(
-            sql.SQL(',').join(map(sql.Identifier, args)),
-            sql.Identifier(table_name)
+        query = sql.SQL("SELECT {} FROM {}").format(
+            sql.SQL(",").join(map(sql.Identifier, args)), sql.Identifier(table_name)
         )
         self.cursor.execute(query)
 
@@ -46,7 +50,7 @@ class PostgresClient:
     def select_all_from_table(self, table_name: str) -> List[Tuple[str, ...]]:
         """Возвращает все значения из переданной таблицы table_name."""
         try:
-            self.cursor.execute('SELECT * FROM {}'.format(table_name))
+            self.cursor.execute("SELECT * FROM {}".format(table_name))
         except errors.SyntaxError:
             raise CantTableError("Вы ввели несуществующее название таблицы.")
 
@@ -55,11 +59,13 @@ class PostgresClient:
     def create_table(self, table_name: str, values_pattern: str) -> None:
         """
         Создаёт новую таблицу table_name с переданными полями и параметрами полей из values_pattern.
-           
-        Поля и параметры values_pattern передаются по шаблону: "test TEXT, test1 VARCHAR(20), test2 INTEGER". 
+
+        Поля и параметры values_pattern передаются по шаблону: "test TEXT, test1 VARCHAR(20), test2 INTEGER".
         """
 
-        query = sql.SQL("CREATE TABLE IF NOT EXISTS {}({})".format(table_name, values_pattern))
+        query = sql.SQL(
+            "CREATE TABLE IF NOT EXISTS {}({})".format(table_name, values_pattern)
+        )
         self.cursor.execute(query)
         self.db_connect.commit()
 
@@ -70,12 +76,43 @@ class PostgresClient:
         Важно, что данные должны быть строками, иначе функция не сработает!
         """
 
-        insert = sql.SQL('INSERT INTO {} ({}) VALUES ({})').format(
+        insert = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
             sql.Identifier(table_name),
-            sql.SQL(', ').join(map(sql.Identifier, kwargs.keys())),
-            sql.SQL(', ').join(map(sql.Literal, kwargs.values())),
+            sql.SQL(", ").join(map(sql.Identifier, kwargs.keys())),
+            sql.SQL(", ").join(map(sql.Literal, kwargs.values())),
         )
         self.cursor.execute(insert)
+        self.db_connect.commit()
+
+    def update_table(self, table_name: str, set_column: str, set_column_value: str) -> None:
+        """Обновляет колонну переданную в set_column данными, переданными в set_column_value."""
+
+        update = sql.SQL("UPDATE {} SET {}={}").format(
+            sql.Identifier(table_name),
+            sql.Identifier(set_column),
+            sql.Literal(set_column_value)
+        )
+        self.cursor.execute(update)
+        self.db_connect.commit()
+    
+    def update_table_where(self, table_name: str, set_column: str, set_column_value: str, where_pattern: str) -> None:
+        """
+           Обновляет колонну переданную в set_column данными, переданными в set_column_value,
+           при условии переданном в where_pattern.
+
+           where_pattern должен соответствовать sql синтаксису после WHERE:
+           "firstname = 'Sasha'"
+           "id = 25"
+            и тп.  
+        """
+
+        update = sql.SQL("UPDATE {} SET {}={} WHERE {}").format(
+            sql.Identifier(table_name),
+            sql.Identifier(set_column),
+            sql.Literal(set_column_value),
+            sql.SQL(where_pattern)
+        )
+        self.cursor.execute(update)
         self.db_connect.commit()
 
     def delete_table(self, table_name: str) -> None:
@@ -83,7 +120,9 @@ class PostgresClient:
 
         query = """
                     DROP TABLE IF EXISTS {} CASCADE;
-                """.format(table_name)
+                """.format(
+            table_name
+        )
         self.cursor.execute(query)
         self.db_connect.commit()
 
@@ -99,11 +138,17 @@ class PostgresClient:
 
         query = """
             DELETE FROM {} WHERE {};
-        """.format(table_name, where_pattern)
+        """.format(
+            table_name, where_pattern
+        )
         self.cursor.execute(query)
         self.db_connect.commit()
 
 
-if __name__ == '__main__':
-    my_postgres_db = PostgresClient(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'),
-                                    password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'))
+if __name__ == "__main__":
+    my_postgres_db = PostgresClient(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+    )
