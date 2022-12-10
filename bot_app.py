3c8b7d3817a8ec67d76f296eb866d8fa19900749
niatpacblack <alexseeffwork@gmail.com>
2022-12-10 22:08:49 +0300

    Рефакторинг black всего проекта. Добавлен каркас бота.

--- bot_app.py
+++ bot_app.py
@@ -3,8 +3,58 @@ import os
 from dotenv import load_dotenv
 from telebot import TeleBot
 
-load_dotenv()
-bot = TeleBot(os.getenv('BOT_TOKEN'))
+from services import get_start_keyboard, get_menu_keyboard, get_admin_keyboard
 
-if __name__ == '__main__':
+load_dotenv()
+bot = TeleBot(os.getenv("BOT_TOKEN"))
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        f"Здравствуйте, {message.from_user.full_name}, выберите действие:",
+        reply_markup=get_start_keyboard(),
+    )
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data == "menu")
+def callback_start(callback):
+    """Выводит категории меню или сообщение об его отсутствии."""
+
+    all_tables_in_db = ...
+
+    if ("menu_categories",) not in all_tables_in_db:
+        bot.send_message(
+            chat_id=callback.message.chat.id,
+            text=f"К сожалению, меню пока не существует.",
+        )
+    else:
+        bot.send_message(
+            chat_id=callback.message.chat.id,
+            text=f"Выберите категорию:",
+            reply_markup=get_menu_keyboard(),
+        )
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data == "admin")
+def callback_admin(callback):
+    """Выводит функционал администратора при проверке chat_id."""
+
+    if callback.message.chat.id not in [
+        int(chat_id) for chat_id in os.getenv("ADMIN_CHAT_ID").split()
+    ]:
+        bot.send_message(
+            chat_id=callback.message.chat.id,
+            text=f"Ваш аккаунт не имеет доступа. Обратитесь к менеджеру заведения.",
+        )
+    else:
+        bot.send_message(
+            chat_id=callback.message.chat.id,
+            text=f"Выберите категорию:",
+            reply_markup=get_admin_keyboard(),
+        )
+
+
+if __name__ == "__main__":
     bot.polling(non_stop=True, interval=0)
--- db.py
+++ db.py
@@ -18,26 +18,30 @@ class PostgresClient:
         self.user = user
         self.password = password
         self.host = host
-        self.db_connect = psycopg2.connect(dbname=self.dbname, user=self.user,
-                                           password=self.password, host=self.host)
+        self.db_connect = psycopg2.connect(
+            dbname=self.dbname, user=self.user, password=self.password, host=self.host
+        )
         self.cursor = self.db_connect.cursor()
 
     def select_all_tables_name_from_db(self) -> List[Tuple[str, ...]]:
         """Выводит список кортежей, содержащий названия всех таблиц из базы данных."""
 
-        self.cursor.execute("""
+        self.cursor.execute(
+            """
                             SELECT table_name FROM information_schema.tables \
                             WHERE table_schema NOT IN ('information_schema','pg_catalog');
-                        """)
+                        """
+        )
 
         return self.cursor.fetchall()
 
-    def select_columns_from_table(self, table_name: str, *args: str) -> List[Tuple[str, ...]]:
+    def select_columns_from_table(
+        self, table_name: str, *args: str
+    ) -> List[Tuple[str, ...]]:
         """Возвращает список с кортежами, содержащими данные переданных полей из *args из таблицы table_name."""
 
-        query = sql.SQL('SELECT {} FROM {}').format(
-            sql.SQL(',').join(map(sql.Identifier, args)),
-            sql.Identifier(table_name)
+        query = sql.SQL("SELECT {} FROM {}").format(
+            sql.SQL(",").join(map(sql.Identifier, args)), sql.Identifier(table_name)
         )
         self.cursor.execute(query)
 
@@ -46,7 +50,7 @@ class PostgresClient:
     def select_all_from_table(self, table_name: str) -> List[Tuple[str, ...]]:
         """Возвращает все значения из переданной таблицы table_name."""
         try:
-            self.cursor.execute('SELECT * FROM {}'.format(table_name))
+            self.cursor.execute("SELECT * FROM {}".format(table_name))
         except errors.SyntaxError:
             raise CantTableError("Вы ввели несуществующее название таблицы.")
 
@@ -55,11 +59,13 @@ class PostgresClient:
     def create_table(self, table_name: str, values_pattern: str) -> None:
         """
         Создаёт новую таблицу table_name с переданными полями и параметрами полей из values_pattern.
-           
-        Поля и параметры values_pattern передаются по шаблону: "test TEXT, test1 VARCHAR(20), test2 INTEGER". 
+
+        Поля и параметры values_pattern передаются по шаблону: "test TEXT, test1 VARCHAR(20), test2 INTEGER".
         """
 
-        query = sql.SQL("CREATE TABLE IF NOT EXISTS {}({})".format(table_name, values_pattern))
+        query = sql.SQL(
+            "CREATE TABLE IF NOT EXISTS {}({})".format(table_name, values_pattern)
+        )
         self.cursor.execute(query)
         self.db_connect.commit()
 
@@ -70,10 +76,10 @@ class PostgresClient:
         Важно, что данные должны быть строками, иначе функция не сработает!
         """
 
-        insert = sql.SQL('INSERT INTO {} ({}) VALUES ({})').format(
+        insert = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
             sql.Identifier(table_name),
-            sql.SQL(', ').join(map(sql.Identifier, kwargs.keys())),
-            sql.SQL(', ').join(map(sql.Literal, kwargs.values())),
+            sql.SQL(", ").join(map(sql.Identifier, kwargs.keys())),
+            sql.SQL(", ").join(map(sql.Literal, kwargs.values())),
         )
         self.cursor.execute(insert)
         self.db_connect.commit()
@@ -83,7 +89,9 @@ class PostgresClient:
 
         query = """
                     DROP TABLE IF EXISTS {} CASCADE;
-                """.format(table_name)
+                """.format(
+            table_name
+        )
         self.cursor.execute(query)
         self.db_connect.commit()
 
@@ -99,11 +107,17 @@ class PostgresClient:
 
         query = """
             DELETE FROM {} WHERE {};
-        """.format(table_name, where_pattern)
+        """.format(
+            table_name, where_pattern
+        )
         self.cursor.execute(query)
         self.db_connect.commit()
 
 
-if __name__ == '__main__':
-    my_postgres_db = PostgresClient(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'),
-                                    password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'))
+if __name__ == "__main__":
+    my_postgres_db = PostgresClient(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+    )
