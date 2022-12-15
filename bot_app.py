addee08d29de6f905fb7fe4b46e91985c0a0deb2
niatpacblack <alexseeffwork@gmail.com>
2022-12-16 02:59:59 +0300

    Добавлен декоратор rewrite_last_message. С помощью него новое сообщение бот отправляет на место старого.

--- bot_app.py
+++ bot_app.py
@@ -24,88 +24,121 @@ from validators import (
 
 load_dotenv()
 bot = TeleBot(os.getenv("BOT_TOKEN"))
+last_message_data = []
+
+
+def rewrite_last_message(func):
+    """Декоратор, созданный для перезаписи последнего сообщения."""
+
+    def wrapper(*args, **kwargs):
+        global last_message_data
+
+        if last_message_data:
+            bot.delete_message(*last_message_data)
+
+        last_message_data = func(*args, **kwargs)
+
+    return wrapper
 
 
 @bot.message_handler(commands=["start"])
+@rewrite_last_message
 def start(message):
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=message.chat.id,
         text=f"Здравствуйте, {message.from_user.full_name}, выберите действие:",
         reply_markup=get_start_keyboard(),
     )
+    return message.chat.id, last_message.id
 
 
 @bot.message_handler(commands=["add_category"])
+@rewrite_last_message
 def add_category(message):
     """Добавляет полученное по шаблону название категории в меню."""
 
     result = add_category_in_menu(message)
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=message.chat.id,
         text=result,
+        reply_markup=get_admin_keyboard(),
     )
+    return message.chat.id, last_message.id
 
 
 @bot.message_handler(commands=["add_dish"])
+@rewrite_last_message
 def add_dish(message):
     """Добавляет полученное по шаблону блюдо в соответствующую категорию меню."""
 
     result = add_dish_in_category(message=message)
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=message.chat.id,
         text=result,
+        reply_markup=get_admin_keyboard(),
     )
+    return message.chat.id, last_message.id
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == "menu")
+@rewrite_last_message
 def callback_menu(callback):
     """Выводит категории меню или сообщение об его отсутствии."""
 
     validation_result = get_menu_validator()
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=callback.message.chat.id,
         text=cb_menu_answer.answer if validation_result else cb_menu_answer.false_answer,
         reply_markup=get_menu_keyboard() if validation_result else None,
     )
+    return callback.message.chat.id, last_message.id
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == "admin")
+@rewrite_last_message
 def callback_admin(callback):
     """Выводит функционал администратора если id чата соответствует зарегистрированному админскому id."""
 
     validation_result = admin_chat_id_validator(callback.message.chat.id)
 
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=callback.message.chat.id,
         text=cb_admin_answer.answer if validation_result else cb_admin_answer.false_answer,
         reply_markup=get_admin_keyboard() if validation_result else None,
     )
+    return callback.message.chat.id, last_message.id
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == "create_menu")
+@rewrite_last_message
 def callback_create_menu(callback):
     """При нажатии кнопки 'Создать меню' создает пустые таблицы для меню в бд и уведомит об этом пользователя."""
 
     create_table_menu_categories()
     create_table_dishes()
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=callback.message.chat.id,
         text=cb_create_menu_answer.answer,
     )
+    return callback.message.chat.id, last_message.id
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == "add_category")
+@rewrite_last_message
 def callback_add_category(callback):
     """Сообщает пользователю в ответном сообщении действия, которые нужно сделать, чтобы добавить новую категорию."""
 
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=callback.message.chat.id,
         text=cb_add_category_answer.answer,
         parse_mode="html",
+        reply_markup=get_admin_keyboard(),
     )
+    return callback.message.chat.id, last_message.id
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == "add_dish")
+@rewrite_last_message
 def callback_add_dish(callback):
     """
     Сообщает пользователю в ответном сообщении действия, которые нужно сделать, чтобы добавить новое блюдо.
@@ -115,22 +148,26 @@ def callback_add_dish(callback):
     categories_data = get_all_categories_data()
     categories_text = get_nice_categories_format(categories_data) if categories_data else 'Доступных категорий нет'
 
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=callback.message.chat.id,
         text=cb_add_dish_answer.answer + f"<i>{categories_text}</i>",
         parse_mode="html",
+        reply_markup=get_admin_keyboard(),
     )
+    return callback.message.chat.id, last_message.id
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == "back_to_start")
+@rewrite_last_message
 def callback_back_to_start(callback):
     """Возвращает пользователя на начальное меню."""
 
-    bot.send_message(
+    last_message = bot.send_message(
         chat_id=callback.message.chat.id,
         text=cb_back_to_start_answer.answer,
         reply_markup=get_start_keyboard(),
     )
+    return callback.message.chat.id, last_message.id
 
 
 if __name__ == "__main__":
